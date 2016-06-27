bbeb5f6275863039aa569bd65300b9ba1b170020
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2016-06-27 13:43:02 +0200

    Returning network object no matter the retry number.

--- app/httpapi-service.js
+++ app/httpapi-service.js
@@ -22,34 +22,28 @@ angular.module('ptAnywhere')
                 sessionUrl = apiUrl + '/sessions/' + sessionId;  // Building the URL manually :-(
             },
             getNetwork: function() {
-                HttpRetry.setSuccess(function(response) {
-                    return response.data;
-                });
+                // We want to process the same function no matter the number of attempt (/retry)
+                var ifSuccess = function(response) { return response.data; };
+                HttpRetry.setSuccess(ifSuccess);
                 return $http.get(sessionUrl + '/network', {timeout: 2000})
-                            .then(HttpRetry.ifSuccess, HttpRetry.responseError);
+                            .then(ifSuccess, HttpRetry.responseError);
             }
         };
     }])
-    .factory('HttpRetry', ['$injector', '$q', '$timeout', '$location',
-                           function($injector, $q, $timeout, $location) {
+    .factory('HttpRetry', ['$http', '$q', '$timeout', '$location',
+                           function($http, $q, $timeout, $location) {
         var tryCount = 0;
         var maxRetries = 5;
         var successCall = null;
 
         function retry(httpConfig, waitForNextRetry) {
             return $timeout(function() {
-                        var $http = $injector.get('$http');
-                        // We want to process the same in any successful attempt
                         return $http(httpConfig).then(successCall, checkError);
                     }, waitForNextRetry);
         }
 
         function checkError(response) {
-            // TODO 404 or 410: sessionExpirationCallback (redirect to not_found)
-            if (response.status === 404 || response.status === 410) {
-                $location.path('/not-found');
-            }
-            else if (response.status === 0 || response.status === 503) {
+            if (response.status === 0 || response.status === 503) {
                 if (tryCount < maxRetries) {
                     var delay = 2000;
                     if (response.status === 0) {  // ERROR_TIMEOUT
@@ -69,8 +63,7 @@ angular.module('ptAnywhere')
             responseError: checkError,
             setSuccess: function(success) {
                 successCall = success;
-            },
-            ifSuccess: successCall
+            }
         };
 
         /*
@@ -89,52 +82,15 @@ angular.module('ptAnywhere')
         }
         */
     }])
-    .factory('HttpErrorsInterceptor', ['$injector', '$q', '$timeout', '$location',
-                                         function($injector, $q, $timeout, $location) {
-        var tryCount = 0;
-        var maxRetries = 5;
-
-        function retry(httpConfig, waitForNextRetry) {
-            return $timeout(function() {
-                        var $http = $injector.get('$http');
-                        return $http(httpConfig);
-                    }, waitForNextRetry);
-        }
-
+    .factory('HttpErrorsInterceptor', ['$q', '$location', function($q, $location) {
         return {
             responseError: function(response) {
                 // TODO 404 or 410: sessionExpirationCallback (redirect to not_found)
                 if (response.status === 404 || response.status === 410) {
                     $location.path('/not-found');
                 }
-                else if (response.status === 0 || response.status === 503) {
-                    var delay = 500;
-                    if (response.status === 0) {  // ERROR_TIMEOUT
-                        console.error('The topology could not be loaded: timeout.');
-                        delay = 0;
-                    }  // Else mark as ERROR_UNAVAILABLE
-                    tryCount++;
-                    return retry(response.config, delay);
-                }
                 // E.g., session has expired and we get error 404 or 410
                 return $q.reject(response);
             }
-            // TODO addListener: function listener for error message:  // or make it watchable by controller
         };
-
-        /*
-        function(tryCount, maxRetries, errorType) {
-            var errorMessage;
-            switch (errorType) {
-                case ptAnywhere.http.UNAVAILABLE:
-                            errorMessage = res.network.errorUnavailable;
-                            break;
-                case ptAnywhere.http.TIMEOUT:
-                            errorMessage = res.network.errorTimeout;
-                            break;
-                default: errorMessage = res.network.errorUnknown;
-            }
-            main.map.error(errorMessage + '. ' + res.network.attempt + ' ' + tryCount + '/' + maxRetries + '.');
-        }
-        */
     }]);
