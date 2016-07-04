481bb4b857f29fd61fec3b0124131da851cef6de
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2016-07-04 15:07:30 +0100

    Returning promises from the controller to networkMap directive.

--- app/controllers.js
+++ app/controllers.js
@@ -25,30 +25,44 @@ angular.module('ptAnywhere')
                 $location.path('/not-found');
             });
     }])
-    .controller('WidgetController', ['$location', '$routeParams', 'baseUrl', 'NetworkMapData', 'PTAnywhereAPIService',
-                                      function($location, $routeParams, baseUrl, mapData, api) {
+    .controller('WidgetController', ['$q', '$location', '$routeParams', 'baseUrl', 'NetworkMapData', 'PTAnywhereAPIService',
+                                      function($q, $location, $routeParams, baseUrl, mapData, api) {
         var self = this;
 
         if (!mapData.isLoaded()) {
             $location.path('/loading/' + $routeParams.id);
         } else {
             self.iconsPath =  baseUrl + '/images/';
-        }
 
-        self.openConsole = function(consoleEndpoint) {
-            var endpoint = 'console?endpoint=' + consoleEndpoint;
-            self.openCmdModal(endpoint);  // Set in the controller
-        };
-        self.onAddDevice = function(x, y) {};
-        self.onAddLink = function(fromDevice, toDevice) {
-            self.openAddLinkModal(fromDevice, toDevice);
-        };
-        self.onEditDevice = function(node) {};
-        self.onDeleteDevice = function(node) {
-            api.removeDevice(node)
-                .catch(function(error) {
-                    console.error('Device removal.', error);
+            self.openConsole = function(consoleEndpoint) {
+                var endpoint = 'console?endpoint=' + consoleEndpoint;
+                self.openCmdModal(endpoint);  // Set in the controller
+            };
+            self.onAddDevice = function(x, y) {};
+            self.onAddLink = function(fromDevice, toDevice) {
+                self.openAddLinkModal(fromDevice, toDevice);
+            };
+            self.onEditDevice = function(node) {};
+            self.onDeleteDevice = function(node) {
+                return $q(function(resolve, reject) {
+                            api.removeDevice(node)
+                                .then(function() {
+                                    resolve();
+                                }, function(error) {
+                                    console.error('Device removal.', error);
+                                    reject();
+                                });
                 });
-        };
-        self.onDeleteLink = function(edge) {};
+            };
+            self.onDeleteLink = function(edge) {
+                return $q(function(resolve, reject) {
+                            api.removeLink(edge).then(function() {
+                                resolve();
+                            }, function() {
+                                console.error('Something went wrong in the link removal.');
+                                reject();
+                            });
+                });
+            };
+        }
     }]);
